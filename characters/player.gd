f19b84aed04bf2a08447edb3d5263cd66014c48e
Arno Bidet <arno.bidet479@gmail.com>
2023-12-05 23:46:15 +0100

    feat: added pickup weapon system and weapon genericity attempt

--- characters/player.gd
+++ characters/player.gd
@@ -1,8 +1,23 @@
 extends CharacterBody2D
+
+signal weapon_switched(prev_index, new_index)
+signal weapon_picked_up(weapon_texture)
+signal weapon_droped(index)
+signal HasShot()
 @export var speed: int = 500  
 
 var bullet = preload("res://weapons/bullet.tscn")
 var canShoot = false
+
+var weapon_inventory = []
+var equipped_weapon_index: int = 0
+var current_weapon: Node2D
+@onready var weapons: Node2D = get_node("Weapons")
+
+func _ready() -> void:
+	if weapons.get_child(0) != null:
+		emit_signal("weapon_picked_up", weapons.get_child(0).get_texture())
+
 
 func shoot():
 	var bullet_instance = bullet.instantiate()
@@ -11,14 +26,11 @@ func shoot():
 
 	bullet_instance.global_position = $Weapon/Muzzle.global_position
 
-	
+
 func Canshoot():
 	canShoot = not canShoot
 
-func _physics_process(delta):
-	var note = get_node("res://hud/note.tscn")
-	#note.CanShoot.connect(Canshoot)
-	
+func _physics_process(_delta):
 	var target_position = get_global_mouse_position()
 	var object_position = global_transform.origin
 	var look = target_position - object_position
@@ -29,14 +41,82 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
-	
-	
-	
-	if Input.is_action_just_pressed("click") and canShoot:
-		shoot()
-		
+	#if current_weapon != null:
+		#print(current_weapon.position)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("click") :
+		if canShoot :
+			shoot()
+		HasShot.emit()
+	if event is InputEventMouseButton :
+		var emb : InputEventMouseButton = event
+		if emb.is_pressed() :
+			if emb.button_index == MOUSE_BUTTON_WHEEL_UP:
+				switch_weapon(MOUSE_BUTTON_WHEEL_UP)
+			if emb.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				switch_weapon(MOUSE_BUTTON_WHEEL_DOWN)
+			
 		
 
 
 func _on_hud_can_shoot(state):
 	canShoot = state
+
+# WEAPON RELATED
+func switch_weapon(direction: int) -> void:
+	var prev_index: int = current_weapon.get_index()
+	var index: int = prev_index
+	if direction == MOUSE_BUTTON_WHEEL_UP:
+		index -= 1
+		if index < 0:
+			index = weapons.get_child_count() - 1
+	else:
+		index += 1
+		if index > weapons.get_child_count() - 1:
+			index = 0
+
+	current_weapon.hide()
+	current_weapon = weapons.get_child(index)
+	current_weapon.show()
+
+	emit_signal("weapon_switched", prev_index, index)
+
+func pick_up_weapon(weapon: Node2D) -> void:
+	weapon_inventory.append(weapon.duplicate())
+	var new_index: int = weapons.get_child_count()
+	weapon.get_parent().call_deferred("remove_child", weapon)
+	weapons.call_deferred("add_child", weapon)
+	weapon.set_deferred("owner", weapons)
+	weapon.set_deferred("position", Vector2(0,0))
+	weapon.set_deferred("offset", Vector2(250,0))
+	weapon.set_deferred("z_index", 200)
+	weapon.get_node("weapon_outlet").set_deferred("scale",Vector2(2,2))
+	
+	
+
+	if current_weapon != null:
+		current_weapon.hide()
+		# current_weapon.cancel_attack()
+	current_weapon = weapon
+
+	emit_signal("weapon_picked_up", weapon.get_texture())
+	emit_signal("weapon_switched", equipped_weapon_index, new_index)
+	
+
+func _drop_weapon() -> void:
+	weapon_inventory.remove_at(current_weapon.get_index() - 1)
+	var weapon_to_drop: Node2D = current_weapon
+	switch_weapon(MOUSE_BUTTON_WHEEL_DOWN)
+
+	emit_signal("weapon_droped", weapon_to_drop.get_index())
+
+	weapons.call_deferred("remove_child", weapon_to_drop)
+	get_parent().call_deferred("add_child", weapon_to_drop)
+	weapon_to_drop.set_owner(get_parent())
+	await weapon_to_drop.tree_entered
+	weapon_to_drop.show()
+
+	var throw_dir: Vector2 = (get_global_mouse_position() - position).normalized()
+	weapon_to_drop.interpolate_pos(position, position + throw_dir * 50)
--- weapons/weapon.gd
+++ weapons/weapon.gd
@@ -1,13 +1,55 @@
-extends AnimatedSprite2D
+extends Node2D
 
+class_name Weapon
+
+@export var on_floor: bool = true
+var player_near: bool = false
+var body: PhysicsBody2D
+var body_position : Vector2
+
+@onready var player_detector: Area2D = get_node("player_detector")
+@onready var pick_up: Sprite2D = get_node("pick_up")
 
 var init_scale = scale
-func _physics_process(delta):
-	look_at(get_global_mouse_position())
-	var target_position = get_global_mouse_position()
-	var object_position = global_transform.origin
-	var look = target_position - object_position
-	if look.x <= 0:
-		set_scale(Vector2(init_scale.x,-init_scale.y))
-	elif look.x > 0 :
-		set_scale(Vector2(init_scale.x,init_scale.y))
+
+func _ready():
+	pick_up.hide()
+
+func _physics_process(_delta):
+	if (not on_floor) :
+		self.position=Vector2(0,0)
+		look_at(get_global_mouse_position())
+		self.position = Vector2(cos(deg_to_rad(global_rotation_degrees)), sin(deg_to_rad(global_rotation_degrees))) * 700
+		print(global_rotation_degrees)
+		var look = get_global_mouse_position() - global_transform.origin
+		if not( global_rotation_degrees <= 90 and global_rotation_degrees >= -90):
+			set_scale(Vector2(init_scale.x,-init_scale.y))
+		else :
+			set_scale(Vector2(init_scale.x,init_scale.y))
+
+func _process(_delta):
+	pass
+
+
+
+func _input(event):
+	if Input.is_key_pressed(KEY_E) and player_near:
+		on_floor = false
+		player_near = false
+		pick_up.hide()
+		body.pick_up_weapon(self)
+			
+func _on_player_detector_body_entered(_body):
+	if on_floor != false :
+		body = _body
+		body_position = body.position
+		player_near = true
+		pick_up.show()
+	
+func _on_player_detector_body_exited(_body):
+	pick_up.hide()
+	player_near = false
+	body = null
+
+func get_texture() -> Texture2D:
+	return get_node("weapon_outlet").texture
