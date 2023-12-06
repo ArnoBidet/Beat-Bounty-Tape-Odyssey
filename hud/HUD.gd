df09ef3c488ef60980d7a07afc28c4ace3efedc9
Arno Bidet <arno.bidet479@gmail.com>
2023-12-06 23:54:41 +0100

    fix: better hud handling

--- hud/HUD.gd
+++ hud/HUD.gd
@@ -1,19 +1,23 @@
 extends CanvasLayer
 signal CanShoot()
 var n = 0
+var group_note = []
 
-func _process(delta):
+func _process(_delta):
 	if( n%50 == 0):
 		var note1 = preload("res://hud/note.tscn").instantiate()
 		add_child(note1)
+		group_note.push_front(note1)
 	n+=1
 	
 
+func _on_player_has_shot():
+	if group_note != null and group_note.size() > 0 :
+		group_note[group_note.size()-1].queue_free()
+		group_note.remove_at(group_note.size()-1)
 
-func _on_area_2d_area_exited(area):
-	CanShoot.emit(false)
-	area.queue_free()
 
-
-func _on_area_2d_area_entered(area):
-	CanShoot.emit(true)
+func _on_note_end_trail_area_entered(area):
+	if group_note != null and group_note.size() > 0 :
+		group_note[group_note.size()-1].queue_free()
+		group_note.remove_at(group_note.size()-1)
