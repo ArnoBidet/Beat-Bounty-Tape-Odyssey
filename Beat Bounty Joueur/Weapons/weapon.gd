e90ef43e4d95c53c916e0c071841ec1ae18a1821
Elyeyes <Elyes_Mathieu@etu.u-bourgogne.fr>
2024-03-17 20:10:56 +0100

    feat: organisation des dossiers
    organistaion des dossiers dans godot selon l'entité, exemple joueur possède les sprites, les scènes et le script de joueur

--- Beat Bounty Joueur/Weapons/weapon.gd
+++ Beat Bounty Joueur/Weapons/weapon.gd
@@ -1,6 +1,9 @@
-extends Node2D
-class_name Health
-@export var max_health = -1 
+extends Node
+@export var on_floor : bool = 1
+@export var can_shoot : bool = 0
+@export var player_near : bool = 0
+@export var equiped : bool = 0
+@export var pickable : bool = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
